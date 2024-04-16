79f23f7d74453bfc461ef35675e3b319234013b1
adarshbinomon <adarshbinomon.1@gmail.com>
2024-04-17 00:32:39 +0530

    5s

--- src/pages/user/UserDetails.tsx
+++ src/pages/user/UserDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
